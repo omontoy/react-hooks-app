253d9ca723aac92e1d8850fca68d501f2bcea888
Orlando Montoya Benitez <orlando.montoyab@perficient.com>
2023-05-12 16:18:53 -0500

    Use firebase to store ingredients data

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -7,11 +7,26 @@ import Search from "./Search";
 function Ingredients() {
   const [userIngredients, setUserIngredients] = useState([]);
 
-  const addIngredientHandler = (newIngredient) => {
-    setUserIngredients((prevIngredients) => [
-      ...prevIngredients,
-      { id: Math.random().toString(), ...newIngredient },
-    ]);
+  const addIngredientHandler = async (newIngredient) => {
+    const response = await fetch(
+      "https://react-hooks-review-b7200-default-rtdb.firebaseio.com/ingredients.json",
+      {
+        method: "POST",
+        body: JSON.stringify(newIngredient),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    if (response.ok) {
+      const data = await response.json();
+
+      setUserIngredients((prevIngredients) => [
+        ...prevIngredients,
+        { id: data.name, ...newIngredient },
+      ]);
+    }
   };
 
   const removeIngredientHandler = (ingredientId) => {
