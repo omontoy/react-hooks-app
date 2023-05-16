a0c0994f5e357ae154e7ecb75bca2c3b7f216a0f
Orlando Montoya Benitez <orlando.montoyab@perficient.com>
2023-05-15 23:06:02 -0500

    Handle errors when fetching and deleting data

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -3,49 +3,60 @@ import React, { useCallback, useState } from "react";
 import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
 import Search from "./Search";
+import ErrorModal from "../UI/ErrorModal";
 
 function Ingredients() {
   const [userIngredients, setUserIngredients] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   const addIngredientHandler = async (newIngredient) => {
     setLoading(true);
-    const response = await fetch(
-      "https://react-hooks-review-b7200-default-rtdb.firebaseio.com/ingredients.json",
-      {
-        method: "POST",
-        body: JSON.stringify(newIngredient),
-        headers: {
-          "Content-Type": "application/json",
-        },
+    try {
+      const response = await fetch(
+        "https://react-hooks-review-b7200-default-rtdb.firebaseio.com/ingredients.json",
+        {
+          method: "POST",
+          body: JSON.stringify(newIngredient),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+
+        setLoading(false);
+
+        setUserIngredients((prevIngredients) => [
+          ...prevIngredients,
+          { id: data.name, ...newIngredient },
+        ]);
       }
-    );
-
-    if (response.ok) {
-      const data = await response.json();
-
-      setLoading(false);
-
-      setUserIngredients((prevIngredients) => [
-        ...prevIngredients,
-        { id: data.name, ...newIngredient },
-      ]);
+    } catch (error) {
+      setError("Something went wrong !!");
     }
   };
 
   const removeIngredientHandler = async (ingredientId) => {
     setLoading(true);
-    await fetch(
-      `https://react-hooks-review-b7200-default-rtdb.firebaseio.com/ingredients/${ingredientId}.json`,
-      {
-        method: "DELETE",
-      }
-    );
-    setLoading(false);
+    try {
+      await fetch(
+        `https://react-hooks-review-b7200-default-rtdb.firebaseio.com/ingredients/${ingredientId}.json`,
+        {
+          method: "DELETE",
+        }
+      );
+      setLoading(false);
 
-    setUserIngredients((prevIngredients) =>
-      prevIngredients.filter((ingredient) => ingredient.id !== ingredientId)
-    );
+      setUserIngredients((prevIngredients) =>
+        prevIngredients.filter((ingredient) => ingredient.id !== ingredientId)
+      );
+    } catch (error) {
+      setError("Something went wrong !!");
+      setLoading(false);
+    }
   };
 
   console.log("INGREDIENTS", userIngredients);
@@ -54,8 +65,13 @@ function Ingredients() {
     setUserIngredients(filteredIngredients);
   }, []);
 
+  const cleanError = () => {
+    setError(null);
+  };
+
   return (
     <div className="App">
+      {error && <ErrorModal onClose={cleanError}>{error}</ErrorModal>}
       <IngredientForm
         onAddIngredient={addIngredientHandler}
         loading={loading}
